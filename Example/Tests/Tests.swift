d2d4a3d0ee38cb827d2a08798152ff1cf8719935
Sean G. Young <sean.g.young@gmail.com>
2016-02-06 17:04:37 -0500

    Added more tests.

--- Example/Tests/Tests.swift
+++ Example/Tests/Tests.swift
@@ -6,8 +6,17 @@ import SGYSwiftJSON
 
 class ComplexObject: JSONCreatableObject {
     
+    convenience init(number: NSNumber) {
+        self.init()
+        self.number = number
+    }
+    
     var number: NSNumber?
     var string: String?
+    
+    var complexObj: ComplexObject?
+    var complexArr: [ComplexObject]?
+    var complexDict: [String: ComplexObject]?
     
 }
 
@@ -21,27 +30,57 @@ class ComplexObjectConversionSpec: QuickSpec {
                 let object = ComplexObject()
                 object.number = 10
                 object.string = "string val"
+                object.complexObj = object
+                
+                object.complexObj = ComplexObject(number: 2)
+                object.complexArr = [ComplexObject(number: 3), ComplexObject(number: 4)]
+                object.complexDict = ["5": ComplexObject(number: 5), "6": ComplexObject(number: 6)]
                 
                 let jsonData = try! serializer.serialize(object)
+                
+//                NSLog("JSON STRING:")
+//                NSLog("\(NSString(data: jsonData, encoding: NSUTF8StringEncoding))")
+//                NSLog("&&& END JSON")
+                
                 let jsonDict = try! NSJSONSerialization.JSONObjectWithData(jsonData, options: [])
                 expect(jsonDict["number"]) == NSNumber(int: 10)
                 expect(jsonDict["string"]) == "string val"
+                
+                expect(jsonDict["complexObj"]??["number"]) == NSNumber(int: 2)
+                
+                expect(jsonDict["complexArr"]??[0]["number"]) == NSNumber(int: 3)
+                expect(jsonDict["complexArr"]??[1]["number"]) == NSNumber(int: 4)
+
+                expect(jsonDict["complexDict"]??["5"]??["number"]) == NSNumber(int: 5)
+                expect(jsonDict["complexDict"]??["6"]??["number"]) == NSNumber(int: 6)
             }
             
             it("will deserialize") {
-                let jsonString = "{\"number\":10, \"string\":\"string val\"}"
-                let jsonData = jsonString.dataUsingEncoding(NSUTF8StringEncoding)!
+                let objPath = NSBundle(forClass: self.dynamicType).pathForResource("ComplexObject", ofType: "json")!
+                let jsonData = NSData(contentsOfURL: NSURL(fileURLWithPath: objPath))!
                 
                 context("into complex object") {
                     let object: ComplexObject = try! deserializer.deserialize(jsonData)
                     expect(object.number) == 10
                     expect(object.string) == "string val"
+                    expect(object.complexObj?.number) == 2
+                    expect(object.complexArr?[0].number) == 3
+                    expect(object.complexArr?[1].number) == 4
+                    expect(object.complexDict?["5"]?.number) == 5
+                    expect(object.complexDict?["6"]?.number) == 6
                 }
                 
                 context("into a dictionary") {
                     let dictionary: [String: AnyObject] = try! deserializer.deserialize(jsonData)
                     expect(dictionary["number"] as? NSNumber) == NSNumber(int: 10)
                     expect(dictionary["string"] as? String) == "string val"
+                    expect(dictionary["complexObj"]?["number"]) == NSNumber(int: 2)
+                    
+                    expect(dictionary["complexArr"]?[0]["number"]) == NSNumber(int: 3)
+                    expect(dictionary["complexArr"]?[1]["number"]) == NSNumber(int: 4)
+                    
+                    expect(dictionary["complexDict"]?["5"]??["number"]) == NSNumber(int: 5)
+                    expect(dictionary["complexDict"]?["6"]??["number"]) == NSNumber(int: 6)
                 }
             }
         }
@@ -79,13 +118,36 @@ class ArrayConverionSpec: QuickSpec {
                 }
                 
                 context("into string array") {
-                    let array: [String] = try! deserializer.deserialize(intArrayJson)
+                    let array: [String] = try! deserializer.deserialize(jsonData)
                     expect(array[0]) == "1"
                     expect(array[1]) == "2"
                     expect(array[2]) == "3"
                 }
+                
+                context("into date array") {
+                    deserializer.dateConversionBlock = { (jsonValue) -> NSDate? in
+                        let unixInterval = (jsonValue as? NSNumber)?.doubleValue ?? 0
+                        return NSDate(timeIntervalSince1970: unixInterval)
+                    }
+                    
+                    let array: [NSDate] = try! deserializer.deserialize(jsonData)
+                    expect(array[0]) == NSDate(timeIntervalSince1970: 1)
+                    expect(array[1]) == NSDate(timeIntervalSince1970: 2)
+                    expect(array[2]) == NSDate(timeIntervalSince1970: 3)
+                }
             }
         }
     }
-    
 }
+
+//class DictionaryConversionSpec: QuickSpec {
+//    override func spec() {
+//        let serializer = SGYJSONSerializer()
+//        let deserializer = SGYJSONDeserializer()
+//        
+//        describe("dictionary will convert properly"
+//    }
+//    
+//    
+//    
+//}
