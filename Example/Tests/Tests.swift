6cba862a30c9d6222993dabc5901a0709ed91076
Sean G Young <syoung@jarustech.com>
2016-02-06 13:49:43 -0500

    Removed boiler plate test code.

--- Example/Tests/Tests.swift
+++ Example/Tests/Tests.swift
@@ -6,45 +6,50 @@ import SGYSwiftJSON
 
 class TableOfContentsSpec: QuickSpec {
     override func spec() {
-        describe("these will fail") {
-
-            it("can do maths") {
-                expect(1) == 2
-            }
-
-            it("can read") {
-                expect("number") == "string"
-            }
-
-            it("will eventually fail") {
-                expect("time").toEventually( equal("done") )
-            }
-            
-            context("these will pass") {
-
-                it("can do maths") {
-                    expect(23) == 23
-                }
-
-                it("can read") {
-                    expect("🐮") == "🐮"
-                }
-
-                it("will eventually pass") {
-                    var time = "passing"
-
-                    dispatch_async(dispatch_get_main_queue()) {
-                        time = "done"
-                    }
-
-                    waitUntil { done in
-                        NSThread.sleepForTimeInterval(0.5)
-                        expect(time) == "done"
-
-                        done()
-                    }
-                }
-            }
+        describe("test passing tests") {
+            it("will pass") { expect(1) == 1 }
         }
+        
+        
+//        describe("these will fail") {
+//
+//            it("can do maths") {
+//                expect(1) == 2
+//            }
+//
+//            it("can read") {
+//                expect("number") == "string"
+//            }
+//
+//            it("will eventually fail") {
+//                expect("time").toEventually( equal("done") )
+//            }
+//            
+//            context("these will pass") {
+//
+//                it("can do maths") {
+//                    expect(23) == 23
+//                }
+//
+//                it("can read") {
+//                    expect("🐮") == "🐮"
+//                }
+//
+//                it("will eventually pass") {
+//                    var time = "passing"
+//
+//                    dispatch_async(dispatch_get_main_queue()) {
+//                        time = "done"
+//                    }
+//
+//                    waitUntil { done in
+//                        NSThread.sleepForTimeInterval(0.5)
+//                        expect(time) == "done"
+//
+//                        done()
+//                    }
+//                }
+//            }
+//        }
     }
 }
